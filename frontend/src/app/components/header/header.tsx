2216b4ad06d0bd204a79932e484c572ee65f867f
HuyHoang130405 <prosdmusic06@gmail.com>
2025-05-08 13:09:55 +0700

    test bootstrap

---
+++ frontend/src/app/components/header/header.tsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+  <>
+  <div className="container-fluid p-5 bg-primary text-white text-center">
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  </>
+  )
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import App from "./components/header/header";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <App/>
         {children}
       </body>
     </html>
