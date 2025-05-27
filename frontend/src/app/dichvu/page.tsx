c3201a53bf94f59ecef6de4c7e7ba13a3e955037
ptthinh131005 <tanthinhphung@gmail.com>
2025-05-27 22:14:28 +0700

    Code Service

---
+++ frontend/src/app/dichvu/page.tsx
@@ -0,0 +1,128 @@
+
+export default function ServicesPage() {
+  return (
+    <>
+    <div className='container' style={{marginTop:'130px',marginBottom:'100px'}}>
+      <h3 className="mb-4">Các dịch vụ có phí</h3>
+      <div className='row gap-4'>
+        <div className="col p-0 border rounded">
+        <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        </div>
+      </div>
+       <div className='row gap-4 mt-4'>
+        <div className="col p-0 border rounded">
+        <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        </div>
+      </div>
+       <div className='row gap-4 mt-4'>
+        <div className="col p-0 border rounded">
+        <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        
+        </div>
+        <div className="col p-0 border rounded">
+          <a  href=""> <img className="w-100 p-2 " style={{borderRadius:'15px'}} src="/img/lau1.jpg" alt="" /></a>
+        <h5 className="mt-3 ms-2">Nhà hàng trong khách sạn</h5>
+        <p className="ms-2">Mô tả về những món ăn ...</p>
+        <div className="border-top"><button className="bg-black  border-0 rounded  text-center w-100 " style={{height:'40px'}}>400.00VND</button></div>
+        </div>
+      </div>
+      <h3 className="mt-5">Các dịch vụ miễn phí</h3>
+      <div className="row p-5">
+       <div className="col">
+        <p> <i className="bi bi-lock-fill"></i>  Khóa ở cửa phòng ngủ</p>
+        <p> <i className="bi bi-basket-fill"></i> Bếp</p>
+        <p> <i className="bi bi-archive-fill"></i> Tủ lạnh</p>
+        <p> <i className="bi bi-fan"></i> Máy điều hòa</p>
+        <p> <i className="bi bi-wrench-adjustable-circle"></i> Mấy sấy tóc</p>
+      </div> 
+       <div className="col">
+         <p> <i className="bi bi-lock-fill"></i>  Khóa ở cửa phòng ngủ</p>
+        <p> <i className="bi bi-basket-fill"></i> Bếp</p>
+        <p> <i className="bi bi-archive-fill"></i> Tủ lạnh</p>
+        <p> <i className="bi bi-fan"></i> Máy điều hòa</p>
+        <p> <i className="bi bi-wrench-adjustable-circle"></i> Mấy sấy tóc</p>
+      </div> 
+       <div className="col">
+         <p> <i className="bi bi-lock-fill"></i>  Khóa ở cửa phòng ngủ</p>
+        <p> <i className="bi bi-basket-fill"></i> Bếp</p>
+        <p> <i className="bi bi-archive-fill"></i> Tủ lạnh</p>
+        <p> <i className="bi bi-fan"></i> Máy điều hòa</p>
+        <p> <i className="bi bi-wrench-adjustable-circle"></i> Mấy sấy tóc</p>
+      </div> 
+       <div className="col">
+         <p> <i className="bi bi-lock-fill"></i>  Khóa ở cửa phòng ngủ</p>
+        <p> <i className="bi bi-basket-fill"></i> Bếp</p>
+        <p> <i className="bi bi-archive-fill"></i> Tủ lạnh</p>
+        <p> <i className="bi bi-fan"></i> Máy điều hòa</p>
+        <p> <i className="bi bi-wrench-adjustable-circle"></i> Mấy sấy tóc</p>
+      </div> 
+      </div>
+    </div>
+
+
+    </>
+
+  );
+}
