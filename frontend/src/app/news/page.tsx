f33035946a93655056bbb54d4c92d788e35d62c5
HuyHoang130405 <prosdmusic06@gmail.com>
2025-05-28 19:56:43 +0700

    News

---
+++ frontend/src/app/news/page.tsx
@@ -0,0 +1,87 @@
+export default function News() {
+    return (
+        <>
+            <div className="container text-white" style={{ marginTop: '130px', marginBottom: '100px' }}>
+                <div className="row border-top border-bottom">
+                    <h1 className="mt-4 mb-4 text-center" style={{ letterSpacing: '10px' }}>TIN TỨC VỀ CHÚNG TÔI</h1>
+                </div>
+                <div className="row border-bottom">
+                    <img className="w-100 mt-3 mb-3" style={{ height: '300px', objectFit: 'cover' }} src="/img/bannernew.png" alt="" />
+                </div>
+                <div className="row">
+                    <h3 className="text-center mt-5 mb-5" style={{ letterSpacing: '5px' }}>MẸO ĐẶT PHÒNG KHÁCH SẠN GIÁ TỐT</h3>
+                    <div className="col">
+                        <p style={{ color: '#FAB320' }}>Ngày 25 tháng 4 năm 2025</p>
+                        <p className="lh-lg fs-6" style={{ textAlign: 'justify' }}>
+                            Grand Ocean Hotel – một trong những thương hiệu khách sạn cao cấp được yêu thích nhất –
+                            chính thức khai trương chi nhánh mới tại thành phố biển Đà Nẵng vào ngày 15/04/2025.
+                            Với vị trí đắc địa gần bãi biển Mỹ Khê, khách sạn hứa hẹn mang đến cho du
+                            khách trải nghiệm nghỉ dưỡng hoàn hảo giữa thiên nhiên trong lành và dịch vụ đẳng cấp.
+                            Chi nhánh mới bao gồm hơn 150 phòng nghỉ hiện đại, hồ bơi vô cực, spa thư giãn,
+                            hà hàng quốc tế và khu vui chơi cho trẻ em. Đặc biệt, trong dịp khai trương, khách sạn áp
+                            dụng ưu đãi giảm giá lên đến 30% cho tất cả các đơn đặt phòng từ nay
+                            đến hết tháng 5, cùng nhiều phần quà hấp dẫn dành cho khách hàng đầu tiên.
+                            Hãy nhanh tay đặt phòng tại Grand Ocean Đà Nẵng để tận hưởng kỳ nghỉ đáng nhớ bên gia đình và người thân!
+                        </p>
+                    </div>
+                    <div className="col">
+                        <img className="w-100" style={{ height: '350px', objectFit: 'cover' }} src="/img/new1.jpg" alt="" />
+                    </div>
+                </div>
+                <div className="row mt-4 border-top">
+                    <div className="col-8 border-end">
+                        <h3 className="mt-4 mb-4" style={{ letterSpacing: '5px' }}>CÁC LOẠI PHÒNG KHÁCH SẠN NỔI TIẾNG</h3>
+                        <p style={{ color: '#FAB320' }}>Ngày 25 tháng 4 năm 2025</p>
+                        <div className="row">
+                            <div className="col">
+                                <img className="w-100" src="/img/new2.jpg" alt="" />
+                            </div>
+                            <div className="col">
+                                <p style={{ textAlign: 'justify', lineHeight: '30px' }}>
+                                    1. Phòng Standard (Tiêu chuẩn)
+                                    Là loại phòng cơ bản nhất, phù hợp với khách du lịch cá nhân hoặc cặp đôi. <br /> <br />
+                                    2. Phòng Deluxe (Cao cấp)
+                                    Diện tích lớn, thường nằm ở tầng cao, có view đẹp (biển, núi, hồ…).<br /> <br />
+                                    3. Phòng Suite (Thượng hạng)
+                                    Là loại phòng cao cấp nhất với không gian sang trọng, thường chia thành nhiều khu: phòng ngủ, phòng khách, bếp nhỏ.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <h3 className="mt-4 mb-4" style={{ letterSpacing: '5px' }}>KINH NGHIỆM DU LỊCH MÙA HÈ</h3>
+                            <p style={{ color: '#FAB320' }}>Ngày 25 tháng 4 năm 2025</p>
+                            <div className="col">
+                                <p style={{ textAlign: 'justify', lineHeight: '30px' }}>
+                                    1. Đặt phòng sớm để tránh "cháy phòng"
+                                    Mùa hè là cao điểm du lịch, đặc biệt ở khu vực ven biển như Đà Nẵng, Nha Trang, Phú <br />
+
+                                    2. Chọn khách sạn có hồ bơi, bữa sáng và gần bãi biển
+                                    Điều này giúp bạn tiết kiệm thời gian di chuyển và tận hưởng kỳ nghỉ tiện lợi, đặc biệt với gia đình có trẻ em. <br />
+
+                                    3. Mang theo kem chống nắng và đồ bơi
+                                    Nắng hè Việt Nam khá gắt, nên bạn cần bảo vệ làn da.
+                                </p>
+                            </div>
+                            <div className="col">
+                                <img className="w-100" src="/img/new3.jpg" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <img className="mt-4 w-100" style={{ height: '380px', objectFit: 'cover' }} src="/img/new4.jpg" alt="" />
+                        <h3 className="mt-4 mb-4" style={{ letterSpacing: '5px' }}>NHỮNG ĐIỀU CẦN BIẾT</h3>
+                        <p style={{ color: '#FAB320' }}>Ngày 25 tháng 4 năm 2025</p>
+                        <p style={{ textAlign: 'justify', lineHeight: '30px' }}>
+                            The Moon Hotel – một trong những thương hiệu khách sạn cao cấp được yêu thích nhất – 
+                            chính thức khai trương chi nhánh mới tại thành phố biển Đà Nẵng vào ngày 15/04/2025. 
+                            Với vị trí đắc địa gần bãi biển Mỹ Khê, khách sạn hứa hẹn mang đến cho du khách trải 
+                            nghiệm nghỉ dưỡng hoàn hảo giữa thiên nhiên trong lành và dịch vụ đẳng cấp.
+                            Chi nhánh mới bao gồm hơn 150 phòng nghỉ hiện đại, hồ bơi vô cực, spa thư giãn, 
+                            nhà hàng quốc tế và khu vui chơi cho trẻ em.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
