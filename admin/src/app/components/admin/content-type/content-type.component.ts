c7186955a0a7d46ec8aec4bd13febbdfee9c13b2
minhchau-ui <phamminhchau0309@gmail.com>
2025-06-18 11:00:20 +0700

    fetch, thêm xoá sửa contentype

--- admin/src/app/components/admin/content-type/content-type.component.ts
+++ admin/src/app/components/admin/content-type/content-type.component.ts
@@ -1,37 +1,94 @@
-// pages/content-type/content-type.component.ts
 import { Component, OnInit } from '@angular/core';
-import { ContentType } from '../../../models/content-type';
 import { ContentTypeService } from '../../../services/content-type.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-content-type',
   standalone: true,
   templateUrl: './content-type.component.html',
-  styleUrls: ['./content-type.component.scss']
+  styleUrls: ['./content-type.component.scss'],
+  imports: [RouterModule, CommonModule, HttpClientModule, FormsModule],
 })
 export class ContentTypeComponent implements OnInit {
-  contentTypes: ContentType[] = [];
+  searchKeyword = '';
+  contentTypeList: any[] = [];
+  filteredContentTypes: any[] = [];
 
-  constructor(private contentTypeService: ContentTypeService) {}
+  isAddPopupOpen = false;
+  isEditPopupOpen = false;
+  isDetailPopupOpen = false;
 
-  ngOnInit(): void {
-    this.getContentTypes();
+  newContentType: any = {};
+  editContentType: any = {};
+  selectedContentType: any = {};
+
+  constructor(private contentTypeService: ContentTypeService) { }
+
+  ngOnInit() {
+    this.getAll();
   }
 
-  getContentTypes() {
-    this.contentTypeService.getAll().subscribe(data => {
-      this.contentTypes = data.data;
-
+  getAll() {
+    this.contentTypeService.getAll().subscribe((res: any) => {
+      this.contentTypeList = res.data.map((type: any) => ({
+        ...type,
+        websitecontents: type.websitecontents || []
+      }));
+      this.filteredContentTypes = res.data.map((type: { website_content_list: any; }) => ({
+        ...type,
+        website_content_list: type.website_content_list || []
+      }));
     });
   }
 
-  onDelete(id: string) {
-    if (confirm('Bạn có chắc chắn muốn xoá?')) {
-      this.contentTypeService.delete(id).subscribe(() => {
-        this.getContentTypes();
-      });
+  onSearch() {
+    const keyword = this.searchKeyword.toLowerCase();
+    this.filteredContentTypes = this.contentTypeList.filter((type) =>
+      type.name.toLowerCase().includes(keyword)
+    );
+  }
+
+  onAdd() {
+    this.newContentType = {};
+    this.isAddPopupOpen = true;
+  }
+
+  closeAddPopup() {
+    this.isAddPopupOpen = false;
+  }
+
+  onAddSubmit() {
+    this.contentTypeService.create(this.newContentType).subscribe(() => {
+      this.closeAddPopup();
+      this.getAll();
+    });
+  }
+
+  onEdit(type: any) {
+    this.editContentType = { ...type };
+    this.isEditPopupOpen = true;
+  }
+
+  onEditSubmit() {
+    this.contentTypeService.update(this.editContentType._id, this.editContentType).subscribe(() => {
+      this.isEditPopupOpen = false;
+      this.getAll();
+    });
+  }
+
+  onDelete(type: any) {
+    if (confirm('Bạn có chắc chắn muốn xóa?')) {
+      this.contentTypeService.delete(type._id).subscribe(() => this.getAll());
     }
   }
 
+  onViewDetail(type: any) {
+    this.selectedContentType = type;
+    this.isDetailPopupOpen = true;
+  }
 }
+
