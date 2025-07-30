d74db199fe3b7924b4a3289c254ae4d632685f4d
Lê Thành Phúc <thanhphuc01102005@gmail.com>
2025-07-30 10:16:23 +0700

    Update test.route.js

--- backend/routes/test.route.js
+++ backend/routes/test.route.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 
-
-console.log("Test route loaded", process.env.BACKEND_URL);
-
+router.get("/", (req, res) => {
+  res.json({
+    message: "Test route is working",
+    backendUrl: process.env.BACKEND_URL,
+    frontendUrl: process.env.FRONTEND_URL,
+    adminUrl: process.env.ADMIN_URL,
+  });
+});
 
 
 module.exports = router;
